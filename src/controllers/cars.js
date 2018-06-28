2124d7b5352295df592a2be6a71694cb8ac0fd59
jewell86 <jewellsmailbox@gmail.com>
2018-06-28 10:33:25 -0700

    create models file code & clean up all code

--- src/controllers/cars.js
+++ src/controllers/cars.js
@@ -1,32 +1,40 @@
-const models = require('../models/cars.js')
+const models = require('../models/cars')
 
-function getAll(req,res,next){
+function getAll (req, res, next) {
     const limit = req.query.limit
     const data = models.getAll(limit)
+    if (!data) return next ({ status:404, message: `No cars found` })
     res.status(200).json({ data })
 }
 
-function getOne(req,res,next){
+function getOne (req, res, next) {
     const id = req.params.id
     const data = models.getOne(id)
+    if (!data) return next ({ status: 404, message: `Car ID ${id} not found` })
     res.status(200).json({ data })
 }
 
-function create(req,res,next){
-    const {make, model} = req.body
-    const data = models.create(make,model)
-    res.status(200).json({ data })
+function create (req, res, next) {
+    const { make, model } = req.body
+    if (!make || !model) return next ({ status: 400, message: `Make & model required` })
+    const data = models.create(make, model)
+    res.status(201).json({ data })
 }
 
-function update(req,res,next){
+function update (req, res, next) {
     const id = req.params.id
-    const {make,model} = req.body
-    const data = models.update(id,make,model)
+    const { make, model } = req.body
+    if (!make || !model) return next ({ status: 400, message: `Make & model required` })
+    const data = models.update(id, make, model)
+    if (!data) return next ({ status: 404, message: `Car ID ${id} not found` })
     res.status(200).json({ data })
 }
 
-function deleteOne(req,res,next){
+function deleteOne (req, res, next) {
     const id = req.params.id
     const data = models.deleteOne(id)
-    res.status(200).json({ data })
+    if (!data) return next({ status: 404, message: `Car ID ${id} not found` })
+    res.status(204).json({ data })
 }
+
+module.exports = { getOne, getAll, create, update, deleteOne }
---
+++ src/models/cars.js
@@ -0,0 +1,35 @@
+const uuid = require('uuid/v4')
+
+const cars = []
+
+function getAll(limit) {
+    return limit ? cars.splice(0,limit) : cars
+}
+
+function getOne(id) {
+    return cars.find(car => car.id === id)
+}
+
+function create(make, model){
+    const car = { id : uuid(), make, model }
+    cars.push(car)
+    return car
+}
+
+function update(id, make, model) {
+    const car = cars.find(car => car.id === id)
+    car.make = make
+    car.model = model
+    return car
+}
+
+function deleteOne(id) {
+    const deletedCar = cars.find(car => car.id === id)
+    cars.filter(car => car !== deletedCar)
+    // const index = cars.indexOf(deletedCar)
+    // cars.splice(index, 1)
+    return `Car ID ${id} has been deleted`
+}
+
+
+module.exports={ getAll, getOne, create, update, deleteOne }
